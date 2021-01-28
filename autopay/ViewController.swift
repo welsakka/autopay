d994320e472011016f7296306d8508db56b4777b
Waleed Elsakka <waleedpahome@live.com>
2021-01-27 23:57:46 -0500

    Initial Commit

---
+++ autopay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autopay
+//
+//  Created by Waleed E on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
