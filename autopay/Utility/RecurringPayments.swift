b5ded805047aa724c11ecb39651cfc3e17858c7d
Waleed Elsakka <waleedpahome@live.com>
2021-10-07 22:16:28 -0400

    Creating skeleton of addRecurringPayment function in index.js

--- autopay/Utility/RecurringPayments.swift
+++ autopay/Utility/RecurringPayments.swift
@@ -6,8 +6,10 @@ class RecurringPayments {
 
     func addRecurringPayment(recPay: RecurringPayment){
         
+        
         var functions = Functions.functions()
         
+        //TODO - Read how to pass a JSON into a firebase functions call using the call() function
         functions.httpsCallable("addRecurringPayment").call() { result, error in
           if let error = error as NSError? {
             if error.domain == FunctionsErrorDomain {
